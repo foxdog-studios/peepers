db7a9ab405f8eb9b6a770064b2956ebf0607456d
Peter Sutton <foxxy@foxdogstudios.com>
2013-01-22 20:28:50 +0000

    Imported Spydroid source, converted to command line tools and removed application class

---
+++ src/net/majorkernelpanic/streaming/Stream.java
@@ -0,0 +1,24 @@
+package net.majorkernelpanic.streaming;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public interface Stream {
+
+	public void start() throws IllegalStateException;
+	public void prepare() throws IllegalStateException,IOException;
+	public void stop();
+	public void release();
+	
+	public void setTimeToLive(int ttl) throws IOException;
+	public void setDestination(InetAddress dest, int dport);
+	
+	public int getLocalPort();
+	public int getDestinationPort();
+	public int getSSRC();
+	
+	public String generateSessionDescriptor() throws IllegalStateException, IOException;
+	
+	public boolean isStreaming();
+	
+}
