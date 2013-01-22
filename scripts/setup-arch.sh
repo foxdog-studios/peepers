f7f6974750d3f43a87df0d7c6c082e9386ff7370
Peter Sutton <foxxy@foxdogstudios.com>
2013-01-22 19:34:01 +0000

    Add more dependencies and improved setup script.

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -3,8 +3,37 @@
 set -o errexit
 set -o nounset
 
-yaourt --needed --noconfirm -Sy \
-        android-ndk \
-        android-sdk \
-        android-sdk-platform-tools
+install_aur=false
+usage='
+    Install dependencies and set up enviroment.
+
+    Usage:
+
+        # setup.sh [-a]
+
+    -a  install AUR packages
+'
+
+while getopts :a opt; do
+    case "${opt}" in
+        a) install_aur=true ;;
+        \?|*)
+            echo "${usage}"
+            exit 1
+            ;;
+    esac
+done
+unset opt usage
+
+if $install_aur; then
+    yaourt --needed --noconfirm -Sy \
+            android-ndk \
+            android-sdk \
+            android-sdk-platform-tools \
+            android-udev
+
+    sudo gpasswd -a "$(whoami)" adbusers
+
+fi
+unset install_aur
 
