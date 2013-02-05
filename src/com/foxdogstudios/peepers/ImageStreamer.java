7e58ac1929c97f21f30bf1331b193852b36f2884
Peter Sutton <foxxy@foxdogstudios.com>
2013-02-05 21:52:27 +0000

    Receiving onPreviewFrame callbacks

--- src/com/foxdogstudios/peepers/ImageStreamer.java
+++ src/com/foxdogstudios/peepers/ImageStreamer.java
@@ -2,6 +2,7 @@ package com.foxdogstudios.peepers;
 
 import java.io.IOException;
 
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -81,8 +82,30 @@ import android.view.SurfaceHolder;
         // Throws RuntimeException if the camera is currently opened
         // by another application.
         final Camera camera = Camera.open();
+        final Camera.Parameters params = camera.getParameters();
+        final Camera.Size previewSize = params.getPreviewSize();
+        final int previewFormat = params.getPreviewFormat();
+        final int BITS_PER_BYTE = 8;
+        final int bytesPerPixel = ImageFormat.getBitsPerPixel(previewFormat) / BITS_PER_BYTE;
+        // XXX: According to the documentation the buffer size can be
+        // calculated by previewSize.width * previewSize.height
+        // * bytesPerPixel. However, this returned an error saying it
+        // was too same. It was always needed to be exactly 1.5 times
+        // larger.
+        final int bufferSize = previewSize.width * previewSize.height * bytesPerPixel * 3 / 2 + 1;
+        final byte[] buffer = new byte[bufferSize];
+        camera.addCallbackBuffer(buffer);
 
-        // Wait for the preview display
+        camera.setPreviewCallbackWithBuffer(new Camera.PreviewCallback()
+        {
+            @Override
+            public void onPreviewFrame(final byte[] data, final Camera camera)
+            {
+                Log.i(TAG, "onPreviewFrame called");
+                camera.addCallbackBuffer(data);
+            } // onPreviewFrame(byte[], Camera)
+        });
+
         synchronized (mLock)
         {
             if (!mRunning)
